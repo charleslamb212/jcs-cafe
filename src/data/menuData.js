bef713204acec193c1e184c3083401da6dce3662
jtw007 <joshwu095@gmail.com>
2023-03-14 14:36:00 -0700

    menu data up to salad added

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -62,12 +62,132 @@ export const menuData = [
         },
 
         omelets_desc: {
+            subheader: 'Omelets are made with 3 eggs and served with hash browns and toast', 
             bam: 'Sirloin chunks, mushrooms, onions, and Swiss cheese',
             denver: 'Ham, onions, green peppers, and cheese', 
             mountain_woman: 'Bacon, onions, peppers, cheese, and tomatoes',
             veggie: 'Onions, tomatoes, mushrooms, peppers, spinach, and cheese',
             mountain_man: 'Sausage, onions, peppers, cheese, and tomatoes', 
             juniper: 'Bacon, onions, spinach, topped with cheese and chili'
-        }
+        },
+
+        omelets_price: {
+            bam: 8.95, 
+            denver: 8.25,
+            cheese: 6.75,
+            mountain_woman: 8.25, 
+            cheese_with_ham: 8.25, 
+            veggie: 8.95, 
+            mountain_man: 8.25, 
+            juniper: 8.75, 
+            cheese_with_sausage: 8.25, 
+            cheese_with_bacon: 8.25
+        }, 
+
+        all_american: {
+            two_pork_eggs: "Two Pork Chops and Eggs",
+            corned_beef: 'Corned Beef hash and Eggs', 
+            two_eggs_bacon: 'Two Eggs with Bacon', 
+            two_eggs: 'Two Eggs',
+            two_eggs_ham: 'Two Eggs with Ham',
+            chicken_fried_steak: 'Chicken Fried Steak and Eggs',
+            sirloin_steak_eggs: 'Sirloin Steak and Eggs',
+            two_eggs_sausage: 'Two Eggs with Sausage Patty'
+        },
+
+        all_american_desc: {
+            subheader: 'Served with hash browns and toast',
+            chicken_fried_steak: 'Topped with choice of green chili or gravy'
+        },
+
+        all_american_price: {
+            two_pork_eggs: 9.25, 
+            corned_beef: 8.25, 
+            two_eggs_bacon: 8.25, 
+            two_eggs: 6.25, 
+            two_eggs_ham: 8.25, 
+            chicken_fried_steak: 9.25, 
+            sirloin_steak_eggs: 9.25, 
+            two_eggs_sausage: 8.25
+        },
+        
+        paul_bunyon: {
+            paul: 'Paul Bunyon'    
+        },
+
+        paul_bunyon_desc: {
+            paul: '1 pancake, 1 French toast, 1 biscuit and gravy, 2 eggs, 2 bacon, 1 sausage patty, and hash browns'
+        },
+
+        paul_bunyon_price: {
+            paul: 9.95
+        },
+
+        biscuits_gravy: {
+            mass_confusion: 'Mass Confusion',
+            two_biscuits_gravy_w_hashbrown: '2 Biscuits and Gravy with Hashbrowns',
+            half_mass: '1/2 Mass',
+            two_biscuits_gravy: '2 Biscuits and Gravy',
+            one_biscuit_gravy: '1 Biscuit and Gravy',
+            one_biscuit_gravy_w_hashbrown: '1 Biscuit and Gravy with Hashbrowns'
+        },
+
+        biscuits_gravy_desc: {
+            mass_confusion: '2 biscuits and gravy piled high hash browns, 2 eggs, and choice of bacon, ham, sausage',
+            half_mass: 'Half the size'
+        },
+
+        biscuits_gravy_price: {
+            mass_confusion: 10.67, 
+            two_biscuits_gravy_w_hashbrown: 5.95,
+            half_mass: 5.95, 
+            two_biscuits_gravy: 4.95, 
+            one_biscuit_gravy: 2.95,
+            one_biscuit_gravy_w_hashbrown: 3.95
+        },
+
+        square_meals: {
+            southern: 'Southern',
+            veggie: 'Veggie',
+            western: 'Western',
+            denver: 'Denver'
+        }, 
+        
+        square_meals_desc: {
+            subheader: 'served in hashbrowns with 2 eggs and toast',
+            southern: 'Sausage, onions, peppers, and cheese',
+            veggie: 'Onions, peppers, tomatoes, mushrooms, spinach, and cheese',
+            western: 'Bacon, onions, peppers, and cheese',
+            denver: 'Ham, onions, peppers, and cheese'
+        }, 
+
+        square_meals_price: {
+            southern: 8.95,
+            veggie: 8.95, 
+            western: 8.95, 
+            denver: 8.95
+        }, 
+
+        from_the_grill: {
+            two_french_toast: '2 Slices of French Toast',
+            pancake: '1 Pancake',
+            full_stack_pancake: 'Full Stack of Pancakes',
+            short_stack_pancake: 'Short Stack of Pancakes',
+            three_french_toast: '3 Slices of French Toast'
+        },
+        
+        from_the_grill_desc: {
+            short_stack_pancake: '2 pancakes',
+            full_stack_pancake: '3 pancakes',
+        },
+        from_the_grill_price: {
+            two_french_toast: 4.95, 
+            pancake: 2.25, 
+            full_stack_pancake: 6.75, 
+            short_stack_pancake: 4.25, 
+            three_french_toast: 6.25
+        },
+
+
     }, 
 ]
