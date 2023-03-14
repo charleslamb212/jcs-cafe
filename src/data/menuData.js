209150df155318d87f12313695a1bb3c2211e24e
jtw007 <joshwu095@gmail.com>
2023-03-14 10:53:54 -0700

    first 2 categories of menu array done

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,6 +1,73 @@
 export const menuData = [
     {
-        name: 'Pancake',
-        desc: 'Delicious pancakes. Served with a river of syrup. '
+        daily_breakfast_specials: {
+            a: "2 Eggs and 2 Bacon Strips",
+            b: "2 Eggs and 1 Sausage Patty",
+            c: "1 Biscuit and Gravy",
+            d: "1/2 Slice of Ham and 2 Eggs",
+            e: "2 Slices of French Toast",
+            f: "Oatmeal",
+            g: "2 Pancakes",
+            h: "1/2 Corned Beef and 2 Eggs"
+        },
+
+        daily_breakfast_special_desc: {
+            a: 'Hash browns and toast', //same for a, b, d, h
+            c: 'Sausage patty',
+            e: '1 Sausage patty',
+            f: 'Toast and small juice',
+            g: 'With 2 strips bacon'
+        },
+
+        daily_breakfast_specials_price: 6.95,
+
+        mexican_breakfast: {
+            huevos_garcia: "Huevos Garcia",
+            two_relleno_and_eggs: "Two Relleno and Eggs",
+            half_breakfast_burrito: "1/2 Breakfast Burrito",
+            two_plus_two: "2+2+2",
+            breakfast_burrito: "Breakfast Burrito",
+            huevos_rancheros: "Huevos Rancheros"
+        },
+
+        mexican_breakfast_desc: {
+            huevos_garcia: '2 eggs, potatos, grilled peppers, onions, tomatoes, smothered in our green chili',
+            two_relleno_and_eggs: 'Smothered in green chili and cheese',
+            half_breakfast_burrito: '2 eggs, cheese, potatoes and choice of ham, bacon, or sausage',
+            two_plus_two: '2 pancakes or 2 French toast, 2 eggs, 2 bacon or 1 sausage patty',
+            breakfast_burrito: '2 eggs, cheese, potatoes, and choice of ham, bacon, or sausage',
+            huevos_rancheros: '2 eggs, over a corn tortilla, refried beans, hash browns smothered in green chili'
+        },
+
+        mexican_breakfast_price: {
+            huevos_garcia: 8.50,
+            two_relleno_and_eggs: 8.95,
+            half_breakfast_burrito: 6.25, 
+            two_plus_two: 7.25,
+            breakfast_burrito: 7.25, 
+            huevos_rancheros: 8.50
+        },
+
+        omelets: {
+            bam: 'Bam-Bam Omelet',
+            denver: 'Denver Omelet',
+            cheese: 'Cheese Omelet',
+            mountain_woman: 'Mountain Woman Omelet',
+            cheese_with_ham: 'Cheese Omelet with Ham', 
+            veggie: 'Veggie Omelet',
+            mountain_man: 'Mountain Man Omelet',
+            juniper: 'Juniper Omelet',
+            cheese_with_sausage: 'Cheese Omelet with Sausage',
+            cheese_with_bacon: 'Cheese Omelet with Bacon'
+        },
+
+        omelets_desc: {
+            bam: 'Sirloin chunks, mushrooms, onions, and Swiss cheese',
+            denver: 'Ham, onions, green peppers, and cheese', 
+            mountain_woman: 'Bacon, onions, peppers, cheese, and tomatoes',
+            veggie: 'Onions, tomatoes, mushrooms, peppers, spinach, and cheese',
+            mountain_man: 'Sausage, onions, peppers, cheese, and tomatoes', 
+            juniper: 'Bacon, onions, spinach, topped with cheese and chili'
+        }
     }, 
 ]
